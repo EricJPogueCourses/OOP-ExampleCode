8793af75bc13c659b04304f308a9e71babb3d9b2
EricJPogue <epogue@epogue.com>
2020-02-18 13:21:35 -0600

    Add files.

---
+++ Random/RandomWorld.java
@@ -0,0 +1,8 @@
+class RandomWorld
+{
+	public static void main(String args[]){ 
+		System.out.println("RandomWorld..."); 
+		int r = Utils.GetNumberBetweenInclusive(1,10);
+		System.out.println(r);
+	}
+}
